61234ca1b2788050a6516abaf7d654ba5d52a451
Sergio Marron <sergiolmarron@gmail.com>
2016-02-27 01:14:19 -0500

    initial setup

--- bin-finder/ViewController.swift
+++ bin-finder/ViewController.swift
@@ -10,16 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var binField: UITextField!
+    
+    @IBOutlet weak var brandLbl: UILabel!
+    
+    @IBOutlet weak var bankLbl: UILabel!
+    @IBOutlet weak var countryLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var cardImg: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
